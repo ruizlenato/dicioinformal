0d7aef84daedb454d747317f4dd7cc0478a21df9
Rian Felipe Celestrino <39718272+RianFC@users.noreply.github.com>
2021-08-28 01:19:02 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='dicioinformal',
+    version='0.5.1',
+    packages=setuptools.find_packages(),
+    url='https://github.com/olixao/dicioinformal',
+    author='OLixao',
+    author_email='contact@olixao.ml',
+    description='search on dicionario informal',
+    long_description='No description for now',
+    long_description_content_type="text/markdown"
+)
